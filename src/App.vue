107e441e0ad65426d63bf78040e88c3af4febf0e
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-04-22 00:39:59 -0600

    Add functionality to next and previous buttons to play music

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,10 @@
         <h2 class="song-title"> {{ current.title }} - <span> {{ current.artist }} </span></h2>
       </section>
       <section class="controls">
-        <button class="prev">Prev</button>
+        <button class="prev" @click="prev">Prev</button>
         <button class="play" v-if="!isPlaying" @click="play">Play</button>
         <button class="pause" v-else @click="pause"> Pause </button>
-        <button class="next">Next</button>
+        <button class="next" @click="next ">Next</button>
       </section>
       <section class="playlist">
         <h3>The Playlist</h3>
@@ -19,7 +19,7 @@
                 :key="song.src" 
                 @click="play(song)"
                 :class="(song.src == current.src) ? 'song playing' : 'song' ">
-          {{ song.title }}
+          {{ song.title }} - {{ song.artist }}
         </button>
       </section>
     </main>
@@ -75,6 +75,24 @@ export default {
     pause () {
       this.player.pause();
       this.isPlaying = false;
+    },
+    next () {
+      this.index++;
+      if(this.index > this.songs.length - 1) {
+        this.index = 0;
+      }
+
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    },
+    prev () {
+      this.index--;
+      if(this.index < 0) {
+        this.index = this.songs.length - 1;
+      }
+
+      this.current = this.songs[this.index];
+      this.play(this.current);
     }
   },
   created () {
